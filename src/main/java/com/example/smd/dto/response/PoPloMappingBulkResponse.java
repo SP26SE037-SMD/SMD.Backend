bfe72867a50fc8e34027a01ff1de4e8748a46948
Shinngaiyeu <taquyminh07022004@gmail.com>
2026-03-30 21:31:32 +0700

    tạo api ở po_plo_mapping

---
+++ src/main/java/com/example/smd/dto/response/PoPloMappingBulkResponse.java
@@ -0,0 +1,23 @@
+package com.example.smd.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PoPloMappingBulkResponse {
+    int deletedCount;
+    int addedCount;
+    List<String> warnings;
+}
