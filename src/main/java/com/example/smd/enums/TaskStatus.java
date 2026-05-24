8c6a82f73767c3a54176fb6bbfd8e93b5b003e41
Taikt1 <taikhuu1102004@gmail.com>
2026-05-24 18:52:19 +0700

    chỉnh websocket

--- src/main/java/com/example/smd/enums/TaskStatus.java
+++ src/main/java/com/example/smd/enums/TaskStatus.java
@@ -1,7 +1,6 @@
 package com.example.smd.enums;
 
 public enum TaskStatus {
-    DRAFT,           // Tạo bản thảo
     TO_DO,           // Chưa bắt đầu
     IN_PROGRESS,     // Đang thực hiện
     DONE,            // Hoàn thành
